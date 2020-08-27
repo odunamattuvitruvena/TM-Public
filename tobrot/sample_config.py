c676c662c89b56551d17659804cca8e150d9702b
odunamattuvitruvena <64315763+odunamattuvitruvena@users.noreply.github.com>
2020-08-27 12:25:28 +0530

    Update sample_config.py

--- tobrot/sample_config.py
+++ tobrot/sample_config.py
@@ -2,14 +2,14 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1173662378:AAH7Xenv07llH2U_T7SBea4OExsvzvMDILE")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1363050429:AAGZjh0i0sawiP82uRHA9K6wQcksVOPboSs")
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", 1667813))
     API_HASH = os.environ.get("API_HASH", "1f6921c27bf6cd01aba471a14ff33bcb")
     OWNER_ID = int(os.environ.get("OWNER_ID", 1326703864))
     # Get these values from my.telegram.org
     # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "-1001189650778").split())
+    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "-1001319532420").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
@@ -34,19 +34,19 @@ class Config(object):
     UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "□  ")
     # add offensive API
     TG_OFFENSIVE_API = os.environ.get("TG_OFFENSIVE_API", None)
-    CUSTOM_FILE_NAME = os.environ.get("CUSTOM_FILE_NAME", "")
-    LEECH_COMMAND = os.environ.get("LEECH_COMMAND", "leech@TM_PrivateBot")
-    YTDL_COMMAND = os.environ.get("YTDL_COMMAND", "ytdl@TM_PrivateBot")
+    CUSTOM_FILE_NAME = os.environ.get("CUSTOM_FILE_NAME", "[TM] ")
+    LEECH_COMMAND = os.environ.get("LEECH_COMMAND", "leech@TM_PublicBot")
+    YTDL_COMMAND = os.environ.get("YTDL_COMMAND", "ytdl@TM_PublicBot")
     RCLONE_CONFIG = os.environ.get("RCLONE_CONFIG", "")
-    DESTINATION_FOLDER = os.environ.get("DESTINATION_FOLDER", "TM_PrivateBot")
-    GLEECH_COMMAND = os.environ.get("GLEECH_COMMAND", "gleech@TM_PrivateBot")
+    DESTINATION_FOLDER = os.environ.get("DESTINATION_FOLDER", "TM_PublicBot")
+    GLEECH_COMMAND = os.environ.get("GLEECH_COMMAND", "gleech@TM_PublicBot")
     INDEX_LINK = os.environ.get("INDEX_LINK", "")
-    TELEGRAM_LEECH_COMMAND_G = os.environ.get("TELEGRAM_LEECH_COMMAND_G", "tleech@TM_PrivateBot")
+    TELEGRAM_LEECH_COMMAND_G = os.environ.get("TELEGRAM_LEECH_COMMAND_G", "tleech@TM_PublicBot")
     CANCEL_COMMAND_G = os.environ.get("CANCEL_COMMAND_G", "cancel")
-    GET_SIZE_G = os.environ.get("GET_SIZE_G", "getsize@TM_PrivateBot")
-    STATUS_COMMAND = os.environ.get("STATUS_COMMAND", "status@TM_PrivateBot")
-    SAVE_THUMBNAIL = os.environ.get("SAVE_THUMBNAIL", "savethumb@TM_PrivateBot")
-    CLEAR_THUMBNAIL = os.environ.get("CLEAR_THUMBNAIL", "clearthumb@TM_PrivateBot")
+    GET_SIZE_G = os.environ.get("GET_SIZE_G", "getsize@TM_PublicBot")
+    STATUS_COMMAND = os.environ.get("STATUS_COMMAND", "status@TM_PublicBot")
+    SAVE_THUMBNAIL = os.environ.get("SAVE_THUMBNAIL", "savethumb@TM_PublicBot")
+    CLEAR_THUMBNAIL = os.environ.get("CLEAR_THUMBNAIL", "clearthumb")
     UPLOAD_AS_DOC = os.environ.get("UPLOAD_AS_DOC", "True")
-    PYTDL_COMMAND_G = os.environ.get("PYTDL_COMMAND_G", "pytdl@TM_PrivateBot")
-    LOG_COMMAND = os.environ.get("LOG_COMMAND", "log@TM_PrivateBot")
+    PYTDL_COMMAND_G = os.environ.get("PYTDL_COMMAND_G", "pytdl@TM_PublicBot")
+    LOG_COMMAND = os.environ.get("LOG_COMMAND", "log@TM_PublicBot")
